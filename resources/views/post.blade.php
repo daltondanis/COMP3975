995263b4fed09dcb9be87c128986fac61ddb26ba
Rebecca <544846169@qq.com>
2016-11-15 00:32:57 -0800

    post.blade.php modified

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,11 +8,11 @@
                         <tr >
                             <td>
                                 <label class="email">Email: </label>
-                                <input class="input" type = "text" name = "">
+                                <input class="input" type = "text" name = "email">
                             </td>
                             <td>
                                 <label class="phone">Phone Number: </label>
-                                <input class="input" type = "text" name = "">
+                                <input class="input" type = "text" name = "phone_number">
                             </td>
                         </tr>
                     </table>
@@ -22,41 +22,41 @@
                         <tr class ="normal_tr_height">
                             <td colspan="3">
                                 <label class="title">Title: </label>
-                                <input class="input_title" type = "text" name = "">
+                                <input class="input_title" type = "text" name = "title">
                             </td>
                         </tr>
                         <tr class ="normal_tr_height">
                             <td>
                                 <label class="small">program: </label>
-                                <input class="small" type = "text" name = "">
+                                <input class="small" type = "text" name = "program">
                             </td>
                             <td>
                                 <label class="small">course: </label>
-                                <input class="small" type = "text" name = "">
+                                <input class="small" type = "text" name = "course">
                             </td>
                             <td>
                                 <label class="teacher">instructor: </label>
-                                <input class="small" type = "text" name = "">
+                                <input class="small" type = "text" name = "instructor">
                             </td>
                         </tr>
                         <tr class="large_tr_height">
                             <td colspan="3">
                                 <label class="title">Description: </label><br>
-                                <input class="input_description" type = "text" name = "">
+                                <textarea class="input_description" name = "description"></textarea>
                             </td>
                         </tr>
                         <tr class ="last_tr_height">
                             <td>
                                 <label class="teacher">Condition: </label>
-                                <input class="small" type = "text" name = "">
+                                <input class="small" type = "text" name = "condition">
                             </td>
                             <td>
                                 <label class="small">Price: </label>
-                                <input class="small" type = "text" name = "">
+                                <input class="small" type = "text" name = "price">
                             </td>
                             <td>
                                 <label class="teacher">Location: </label>
-                                <input class="small" type = "text" name = "">
+                                <input class="small" type = "text" name = "location">
                             </td>
                         </tr>
                     </table>
