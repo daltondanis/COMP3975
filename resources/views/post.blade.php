e183ca9910cb2e883259828671f352796124a600
Arv91 <arvin_rinaldo@yahoo.com>
2016-11-26 23:54:36 -0800

    Updated post page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,78 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-    <style>
-        .table {
-            max-width: 90%;
-            table-layout: fixed;
-            border: hidden;
-        }
-    </style>
-    <div class="content">
-        <form method="POST" enctype="multipart/form-data">
-                <div class="description">
-                    <table class = "table note_info table-responsive">
-                        <tr class ="normal_tr_height">
-                        <td colspan="2">
-                            <label class="small">Title: </label>
-                            <input class="form-control" type = "text" name = "title">
-                        </td>
-                        <td>
-							<div class="uploadbutton">
-								<input type="file" name="myImage" id="file" class="inputfile" required />
-								<label for="file" class="small2">upload</label>
-							</div>
-                            <input type="text" name="fileName" value="" class="myinput form-control filename" readonly>
-                        </td>
-                        </tr>
-                        <tr class ="normal_tr_height">
-                            <td class="first">
-                                <label class="small">school: </label>
-                                <select class="form-control t" name="schools">
-                                    @foreach($schools as $key => $school)
-                                        <option class="options" value="{{$key}}">{{$school}}</option>
-                                    @endforeach
-                                </select><br>
-                            </td>
-                            <td>
-                                <label class="small">course: </label>
-                                <input class="small form-control" type = "text" name = "course">
-                            </td>
-                        </tr>
-                        <tr class ="normal_tr_height">
-                            <td class="first">
-                                <label class="small">year: </label>
-                                <input class="small form-control" type = "number" value="2016" name = "yearTaken">
-                            </td>
-                            <td>
-                                <label class="small">instructor: </label>
-                                <input class="small form-control" type = "text" name = "instructor">
-                            </td>
-                            <td>
-                                <label class="small">price: </label>
-                                <input class="small form-control" type = "number" name = "price">
-                            </td>
-                        </tr>
-                        <tr class="large_tr_height">
-                            <td colspan="2">
-                                <label class="title">Description: </label><br>
-                                <textarea class="input_description form-control" name = "description"></textarea>
-                            </td>
-                            <td colspan="1">
-                            <label class="small">program: </label>
-                            <input class="small form-control" type = "text" name = "program">
-                            </td>
-                        </tr>
-                    </table>
+    <form method="POST" enctype="multipart/form-data">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                    <label class="small">Title: </label>
+                    <input class="form-control" type="text" name="title">
                 </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">Program: </label>
+                    <input class="small form-control" type ="text" name="program">
+                </div>
+                
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">School: </label>
+                    <select class="form-control" name="schools">
+                        @foreach($schools as $key => $school)
+                            <option class="options" value="{{$key}}">{{$school}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">Course: </label>
+                    <input class="small form-control" type ="text" name="course">
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">Year: </label>
+                    <input class="small form-control" type ="number" value="2016" name="yearTaken">
+                </div>
+                
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">Instructor: </label>
+                    <input class="small form-control" type="text" name="instructor">
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">Price: </label>
+                    <input class="small form-control" type="number" name="price">
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label class="small">Image: </label>
+                    <input class="form-control" type="file" name="myImage" required>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <label class="title">Description: </label><br>
+                    <textarea class="input_description form-control" name="description"></textarea>
+                </div>
+            </div>
+        </div>
 
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="hidden" name="user" value=" {{ \Auth::user()->id }}">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="user" value=" {{ \Auth::user()->id }}">
 
-                <div class = "button">
-                    <input type="reset" value="Reset">
+        <div class = "button">
+            <input type="reset" value="Reset">
                     <input class="submit" type="submit" name="submit" value=" ">
-                </div>
-            </form>
-    </div>
+        </div>
+    </form>
 @endsection
